a1a14db04f195256a4def94afa266db133f401cd
honwen.chan <honwen.chan@gmail.com>
2021-07-06 19:53:47 +0800

    update data: all

--- dnsmasq-extra/files/data/gzip.sh
+++ dnsmasq-extra/files/data/gzip.sh
@@ -4,10 +4,14 @@ curl -sSL https://ispip.clang.cn/all_cn_cidr.txt -o chnroute.txt
 md5sum chnroute.txt | tee chnroute.txt.md5sum
 
 # https://github.com/honwen/shadowsocks-helper
-shadowsocks-helper gfwlist > gfwlist
+shadowsocks-helper gfwlist >gfwlist
 
 curl -sSL https://anti-ad.net/domains.txt -o adblock
 shadowsocks-helper tide -i adblock -o adblock
+
+# whitelist
+sed '/ip-api.com/d; /pv.sohu.com/d' -i adblock
+sed '/click.union.vip.com/d; /ms.vipstatic.com/d' -i adblock
 
 srcs="tldn gfwlist direct adblock"
 
