5d2293a0f71904ef5d9ed2c090bc61739342ebc7
honwen.chan <honwen.chan@gmail.com>
2021-12-31 21:03:06 +0800

    dnsmasq-extra: refine

--- dnsmasq-extra/files/data/generate.sh
+++ dnsmasq-extra/files/data/generate.sh
@@ -5,19 +5,44 @@
 # https://github.com/honwen/shadowsocks-helper
 # https://github.com/zhanhb/cidr-merger
 
+_date=$(date '+%Y-%m-%d')
+_path=$(dirname $(dirname $(dirname $(readlink -f $0))))
+
+sed "s+PKG_VERSION:=.*+PKG_VERSION:=${_date}+g" -i $_path/Makefile
+echo "# Generated At ${_date}"
+
+cd $_path/files/data
 # ------------------ chnroute ------------------
 time cidr-merger <<-EOF >chnroute.txt.new
-	$(curl -sSL https://ispip.clang.cn/all_cn_cidr.txt)
-	$(curl -sSL https://cdn.staticaly.com/gh/pexcn/daily/gh-pages/chnroute/chnroute.txt)
-	$(curl -sSL https://raw.githubusercontents.com/pexcn/daily/gh-pages/chnroute/chnroute.txt)
-	$(curl -sSL https://cdn.staticaly.com/gh/17mon/china_ip_list/master/china_ip_list.txt)
-	$(curl -sSL https://raw.githubusercontents.com/17mon/china_ip_list/master/china_ip_list.txt)
+	$(curl -skL --speed-limit 100000 --speed-time 10 https://ispip.clang.cn/all_cn_cidr.txt)
+
+	$(curl -skL --speed-limit 100000 --speed-time 10 https://raw.githubusercontent.com/metowolf/iplist/master/data/country/CN.txt)
+
+	$(
+		curl -skL --speed-limit 100000 --speed-time 10 https://cdn.staticaly.com/gh/pexcn/daily/gh-pages/chnroute/chnroute.txt ||
+			curl -skL --speed-limit 100000 --speed-time 10 https://raw.githubusercontent.com/pexcn/daily/gh-pages/chnroute/chnroute.txt
+	)
+
+	$(
+		curl -skL --speed-limit 100000 --speed-time 10 https://cdn.staticaly.com/gh/17mon/china_ip_list/master/china_ip_list.txt ||
+			curl -skL --speed-limit 100000 --speed-time 10 https://raw.githubusercontent.com/17mon/china_ip_list/master/china_ip_list.txt
+	)
+
+	$(
+		curl -skL --speed-limit 100000 --speed-time 10 https://cdn.staticaly.com/gh/QiuSimons/Chnroute/master/dist/chnroute/chnroute.txt ||
+			curl -skL --speed-limit 100000 --speed-time 10 https://raw.githubusercontent.com//QiuSimons/Chnroute/master/dist/chnroute/chnroute.txt
+	)
+
+	$(
+		curl -skL --speed-limit 100000 --speed-time 10 https://cdn.staticaly.com/gh/gaoyifan/china-operator-ip/ip-lists/china.txt ||
+			curl -skL --speed-limit 100000 --speed-time 10 https://raw.githubusercontent.com/gaoyifan/china-operator-ip/ip-lists/china.txt
+	)
+
 	$(shadowsocks-helper asn)
 EOF
 
 # add checkip.synology.com
-time cidr-merger <<-EOF >chnroute.txt
-	$(cat chnroute.txt.new)
+time cidr-merger <<-EOF >>chnroute.txt.new
 	104.248.79.120
 	159.89.129.146
 	165.227.63.200
@@ -26,6 +51,12 @@ time cidr-merger <<-EOF >chnroute.txt
 	206.189.214.49
 	142.93.81.166
 	159.65.77.153
+EOF
+
+# min netmask >= /24
+time cidr-merger <<-EOF >chnroute.txt
+	$(cat chnroute.txt.new)
+	$(sed 's+\.[^\.]*$+.0/24+g' chnroute.txt.new)
 EOF
 
 sed '/^[ \t\s]*$/d' -i chnroute.txt
@@ -38,17 +69,31 @@ sed '/^google.*analytics.com$/d' -i gfwlist
 # ------------------ gfwlist ------------------
 
 # ------------------ adblock ------------------
+
 curl -sSL https://anti-ad.net/domains.txt -o adblock
+curl -sSL https://raw.githubusercontent.com/neodevpro/neodevhost/master/customblocklist >>adblock
+# curl -sSL https://neodev.team/lite_host -o adblock.neodevhost
+# $(sed -n 's+^0.0.0.0 *++p' adblock.neodevhost)
+cat <<-EOF >>adblock
+	c.msn.com
+	ntp.msn.com
+	ntp.msn.cn
+	assets.msn.cn
+	api.msn.com
+	browser.events.data.msn.com
+EOF
+# sort -u adblock -o adblock
 time shadowsocks-helper tide -i adblock -o adblock
 
 # whitelist
 sed '/wns.windows.com/d' -i adblock
 sed '/ip-api.com/d; /pv.sohu.com/d' -i adblock
 sed '/click.union.vip.com/d; /ms.vipstatic.com/d' -i adblock
+# rm -f adblock.*
 # ------------------ adblock ------------------
 
 # ------------------ direct ------------------
-curl -sSL https://raw.githubusercontents.com/pexcn/daily/gh-pages/chinalist/chinalist.txt -o direct.pexcn
+curl -sSL https://raw.githubusercontent.com/pexcn/daily/gh-pages/chinalist/chinalist.txt -o direct.pexcn
 
 start=$(($(sed -n -e '/^whatismyip.akamai.com$/=' direct) + 1))
 cat <<-EOF | sort -u >direct.new
@@ -89,3 +134,35 @@ for it in $srcs; do
 	md5sum $it.gz | tee $it.gz.md5sum
 done
 # ------------------ gzip ------------------
+
+# ----------- ShadowrocketEx.conf ----------
+
+rm -f ShadowrocketEx.conf Complete.conf
+curl -sSL https://raw.githubusercontent.com/lhie1/Rules/master/Shadowrocket/Complete.conf -o Complete.conf
+sed 's+^dns-server.*+dns-server = tls://223.5.5.5, https://doh.pub/dns-query, https://223.6.6.6/dns-query+g' -i Complete.conf
+sed 's+^bypass-tun *= *+bypass-tun = 223.5.5.0/24, 223.6.6.0/24, 120.53.80.0/24, 175.24.219.0/24, 162.14.20.0/22, +g' -i Complete.conf
+
+end_of_header=$(sed -n -e '/^DOMAIN-SUFFIX,12306.cn,DIRECT$/=' Complete.conf)
+start_of_cidr=$(sed -n -e "$end_of_header,999999{/^IP-CIDR,.*,DIRECT,no-resolve$/=}" Complete.conf | head -n1)
+
+echo "# Generated At ${_date}" >ShadowrocketEx.conf
+sed -n "1,$((end_of_header - 1))p" Complete.conf >>ShadowrocketEx.conf
+
+(
+	sed -n "$end_of_header,$((start_of_cidr - 1))p" Complete.conf
+	sed -n "$start,999999p" direct | sed 's+^+DOMAIN-SUFFIX,+g; s+$+,DIRECT,no-resolve+g'
+) | sort -u | sed '/^[ \t\s]*$/d' >>ShadowrocketEx.conf
+
+sed 's+^+IP-CIDR,+g; s+$+,DIRECT,no-resolve+g' chnroute.txt >>ShadowrocketEx.conf
+echo -e '\n# > GFWLIST' >>ShadowrocketEx.conf
+sed '/[0-9]$/d' gfwlist | sed 's+^+DOMAIN-SUFFIX,+g; s+$+,PROXY+g' >>ShadowrocketEx.conf
+echo -e '\n# > CN' >>ShadowrocketEx.conf
+
+start_of_footer=$(sed -n -e '/^DOMAIN-SUFFIX,cn,DIRECT$/=' Complete.conf)
+
+sed -n "$start_of_footer,999999p" Complete.conf >>ShadowrocketEx.conf
+
+rm -f Complete.conf
+# ----------- ShadowrocketEx.conf ----------
+
+cd -
