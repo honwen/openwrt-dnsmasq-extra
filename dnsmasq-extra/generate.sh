4efcc78e8e0645ebfaf91fe79da5f140d582e7b7
honwen.chan <honwen.chan@gmail.com>
2024-12-21 22:22:46 +0800

    update data: all @2024.1221

--- dnsmasq-extra/generate.sh
+++ dnsmasq-extra/generate.sh
@@ -10,11 +10,15 @@ _date=$(date '+%Y-%m-%d')
 _path=$(dirname $(readlink -f $0))
 
 curl_githubusercontent() {
-	url="$1"
-	curl -skL --speed-limit 100000 --speed-time 10 "https://ghproxy.com/${url}" ||
-		curl -skL --speed-limit 100000 --speed-time 10 "https://ghproxy.net/${url}" ||
-		curl -skL --speed-limit 100000 --speed-time 10 "$(echo ${url} | sed 's+raw.githubusercontent.com+cdn.staticaly.com/gh+g')" ||
-		curl -skL --speed-limit 100000 --speed-time 10 "${url}"
+	url="$(echo $1 | sed 's+^https*://++g')"
+	curl -skSL --speed-limit 100000 --speed-time 10 "https://${url}" ||
+		curl -skSL --speed-limit 100000 --speed-time 10 "https://ghproxy.com/https://${url}" ||
+		curl -skSL --speed-limit 100000 --speed-time 10 "https://ghproxy.net/https://${url}" ||
+		curl -skSL --speed-limit 100000 --speed-time 10 "https://ghproxy.cc/https://${url}" ||
+		curl -skSL --speed-limit 100000 --speed-time 10 "https://ghproxy.cn/https://${url}" ||
+		curl -skSL --speed-limit 100000 --speed-time 10 "https://ghp.ci/https://${url}" ||
+		curl -skSL --speed-limit 100000 --speed-time 10 "https://files.m.daocloud.io/${url}" ||
+		curl -skSL --speed-limit 100000 --speed-time 10 "$(echo https://${url} | sed 's+raw.githubusercontent.com+cdn.staticaly.com/gh+g')"
 }
 
 sed "s+PKG_VERSION:=.*+PKG_VERSION:=${_date}+g" -i $_path/Makefile
@@ -31,7 +35,7 @@ sort -u tldn -o tldn
 # ------------------ chnroute ------------------
 echo >&2 "# chnroute.txt"
 time cidr-merger <<-EOF >chnroute.txt.new
-	$(curl -skL --speed-limit 100000 --speed-time 10 https://ispip.clang.cn/all_cn_cidr.txt)
+	$(curl -skSL --speed-limit 100000 --speed-time 10 https://ispip.clang.cn/all_cn_cidr.txt)
 
 	$(curl_githubusercontent https://raw.githubusercontent.com/metowolf/iplist/master/data/country/CN.txt)
 
@@ -44,6 +48,8 @@ time cidr-merger <<-EOF >chnroute.txt.new
 	$(curl_githubusercontent https://raw.githubusercontent.com/gaoyifan/china-operator-ip/ip-lists/china.txt)
 
 	$(curl_githubusercontent https://raw.githubusercontent.com/PaPerseller/chn-iplist/master/chnroute-ipv4.txt)
+
+	$(curl_githubusercontent https://raw.githubusercontent.com/soffchen/GeoIP2-CN/release/CN-ip-cidr.txt)
 
 	$(
 		curl_githubusercontent https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/ChinaCompanyIp.list |
@@ -60,12 +66,17 @@ time cidr-merger <<-EOF >chnroute.txt.new
 			sed -n 's+IP-CIDR,\(.*\),no-resolve+\1+p'
 	)
 
-	$(for it in 3462 9269 9381 25820 31898 45090 45102 48266 64050 132203 132591 135377 136038 136907 138915 141159; do
+	154.21.192.0/20
+
+	$(for it in 3462 9269 9381 25820 31898 35916 36352 45090 45102 48266 49683 64050 132203 132591 135377 136038 136907 138915 141159; do
 		echo >&2 "ASN$it"
-		echo
+
+		curl_githubusercontent https://raw.githubusercontent.com/cbuijs/ipasn/refs/heads/master/asn/as$it.list | grep -vE '^#' | grep -v ':'
+
+		# curl -skSL -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64)' --speed-limit 50000 --speed-time 90 https://api.bgpview.io/asn/$it/prefixes | jq . >../../.asn/$it.json
+		# sleep 44
+
 		jq -r '.data.ipv4_prefixes[]|.prefix' ../../.asn/$it.json
-		# curl -skL --speed-limit 100000 --speed-time 30 https://api.bgpview.io/asn/$it/prefixes | jq -r '.data.ipv4_prefixes[]|.prefix' 2>/dev/null ||
-		#  curl -skL --speed-limit 50000 --speed-time 90 https://api.bgpview.io/asn/$it/prefixes | jq -r '.data.ipv4_prefixes[]|.prefix'
 	done)
 EOF
 
@@ -112,6 +123,11 @@ rm -f chnroute.txt.*
 echo >&2 "# gfwlist"
 time shadowsocks-helper gfwlist >gfwlist
 sed '/^google.*analytics.com$/d' -i gfwlist
+sed '/^apple.com$/d' -i gfwlist
+echo 'ess.apple.com' >>gfwlist
+echo 'push.apple.com' >>gfwlist
+curl_githubusercontent https://raw.githubusercontent.com/dler-io/Rules/main/Surge/Surge%203/Provider/OpenAI.list |
+	grep -v 'KEYWORD' | grep '^DOMAIN' | awk -F',' '{print $2}' >>gfwlist
 # ------------------ gfwlist ------------------
 
 # ------------------ gfwlist.lite ------------------
@@ -124,11 +140,13 @@ echo
 
 # ------------------ adblock ------------------
 
-curl -sSL https://anti-ad.net/domains.txt -o adblock
-curl_githubusercontent https://raw.githubusercontent.com/VeleSila/yhosts/master/hosts | sed -n 's+^0.0.0.0 *++p' >adblock.lite
+curl_githubusercontent https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/adblockdomain.txt | grep -vE '^!' >adblock
+curl_githubusercontent https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/adblockdomainlite.txt | grep -vE '^!' >adblock.lite
+
+curl_githubusercontent https://raw.githubusercontent.com/neodevpro/neodevhost/master/domain | sed -n '/^#/d' >>adblock
 curl_githubusercontent https://raw.githubusercontent.com/neodevpro/neodevhost/master/customblocklist | tee -a adblock adblock.lite >/dev/null
-curl_githubusercontent https://raw.githubusercontent.com/code-shiromi/Quantumult-X-Resources/main/remote/filters/ad.list |
-	sed -n 's+^HOST.*,\(.*\),AdBlock$+\1+p' | tee -a adblock adblock.lite >/dev/null
+curl_githubusercontent https://raw.githubusercontent.com/privacy-protection-tools/anti-AD/refs/heads/master/discretion/pcdn.txt | grep -v '^#' | tee -a adblock adblock.lite >/dev/null
+curl_githubusercontent https://raw.githubusercontent.com/privacy-protection-tools/anti-AD/refs/heads/master/discretion/anv.txt | grep -v '^#' | tee -a adblock adblock.lite >/dev/null
 cat <<-EOF | tee -a adblock adblock.lite >/dev/null
 	c.msn.com
 	ntp.msn.com
@@ -137,30 +155,37 @@ cat <<-EOF | tee -a adblock adblock.lite >/dev/null
 	api.msn.com
 	browser.events.data.msn.com
 EOF
+grep -v '^#' adblock.ext | sort -u | tee -a adblock adblock.lite >/dev/null
 
 echo >&2 "# adblock"
 time shadowsocks-helper tide -i adblock -o adblock
-echo >&2 "# adblock.lite"
-time shadowsocks-helper tide -i adblock.lite -o adblock.lite_whitelist
-grep -Ex -f adblock.lite_whitelist adblock >adblock.lite
+time shadowsocks-helper tide -i adblock.lite -o adblock.lite
 
 # whitelist
+curl_githubusercontent https://raw.githubusercontent.com/217heidai/adblockfilters/refs/heads/main/rules/white.txt |
+	grep -v '^#' | grep -E '^[a-z\-\.]*$' | while read it; do
+	sed "/^${it}$/d" -i adblock adblock.lite
+done
 sed 's+\.$++g' -i adblock adblock.lite
+sed 's+localhost++g' -i adblock adblock.lite
 sed '/^bj.bcebos.com/d; /^puui.qpic.cn/d; /^zhanzhang.toutiao.com/d' -i adblock adblock.lite
+sed '/tencent-cloud/d' -i adblock adblock.lite
 sed '/weixinbridge/d' -i adblock adblock.lite
 sed '/bootcdn.net/d' -i adblock adblock.lite
 sed '/wns.windows.com/d' -i adblock adblock.lite
+sed '/e.189.cn/d' -i adblock adblock.lite
 sed '/ip-api.com/d; /pv.sohu.com/d' -i adblock adblock.lite
 sed '/click.simba.taobao.com/d' -i adblock adblock.lite
 sed '/click.union.vip.com/d; /ms.vipstatic.com/d' -i adblock adblock.lite
-rm adblock.lite_*
+sed '/dlswbr.baidu.com/d' -i adblock adblock.lite
+# rm adblock.lite_*
+
 # ------------------ adblock ------------------
 
 # ------------------ direct ------------------
 curl_githubusercontent https://raw.githubusercontent.com/pexcn/daily/gh-pages/chinalist/chinalist.txt >direct.pexcn
 
 start=$(($(sed -n -e '/^whatismyip.akamai.com$/=' direct) + 1))
-# $(sed -n "$start,99999p" direct)
 cat <<-EOF | sort -u >direct.new
 	$(sed '/^www.apple.com$/,+99999d' direct.pexcn)
 EOF
@@ -170,16 +195,15 @@ curl_githubusercontent https://raw.githubusercontent.com/Loyalsoldier/surge-rule
 curl_githubusercontent https://raw.githubusercontent.com/v2fly/domain-list-community/master/data/tencent >>direct.new
 curl_githubusercontent https://raw.githubusercontent.com/v2fly/domain-list-community/master/data/alibaba >>direct.new
 curl_githubusercontent https://raw.githubusercontent.com/v2fly/domain-list-community/master/data/bytedance >>direct.new
+curl_githubusercontent https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/China/China_Domain.txt | sed '/^#/d' | sed '/akadns.net/d; /microsoft/d' >>direct.new
 curl_githubusercontent https://raw.githubusercontent.com/pluwen/china-domain-allowlist/main/allow-list.sorl |
 	sed -n 's+^*\.++p' | sed '/apple/d; /akadns/d; /doubleclick/d' >>direct.new
 curl_githubusercontent https://raw.githubusercontent.com/eliozy/Qumtumult-X/master/Filter/WeChat.list |
 	sed -n 's+^DOMAIN-SUFFIX,++p' | sed 's+,.*++g' >>direct.new
-curl_githubusercontent https://github.com/ACL4SSR/ACL4SSR/blob/master/Clash/Ruleset/Wechat.list |
+curl_githubusercontent https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/Ruleset/Wechat.list |
 	sed -n 's+^DOMAIN[^,]*,++p' >>direct.new
 curl_githubusercontent https://raw.githubusercontent.com/marsgogo/Surge/main/Weixin.list |
 	sed -n 's+^DOMAIN[^,]*,++p' >>direct.new
-curl_githubusercontent https://raw.githubusercontent.com/JC-SYSU/test/main/WhiteList.list |
-	awk -F',' '{print $2}' | sed '/[0-9]$/d' >>direct.new
 curl_githubusercontent https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/ChinaMedia.list |
 	sed -n 's+^DOMAIN[^,]*,++p' >>direct.new
 curl_githubusercontent https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/ChinaDomain.list |
@@ -196,8 +220,9 @@ sed '/google/d; /gstatic/d; /youtube/d; /^android/d;' -i direct.new
 sed '/ocsp/d; /akamai/d; /aws/d' -i direct.new
 sed '/sci-hub/d; /scihub/d; /gitbook/d' -i direct.new
 sed '/ebay/d; /lazada/d; /yandex/d' -i direct.new
-cat adblock adblock.lite gfwlist >direct.blacklist
+cat adblock gfwlist >direct.blacklist
 sed "$start,99999d" direct >>direct.blacklist
+sed 's+\.+\\.+g' -i direct.blacklist
 grep -Fv -f direct.blacklist direct.new >direct.sum
 
 echo >&2 "# direct.sum"
@@ -210,6 +235,7 @@ echo "\.*microsoft\." >>direct.suffix
 echo "\.*windowsupdate\." >>direct.suffix
 echo >&2 "# direct"
 time grep -eF -f direct.suffix direct.sum >>direct
+sed '/gvt2.com/d; /www.microsoft.com/d' -i direct
 rm -f direct.*
 # ------------------ direct ------------------
 
@@ -229,6 +255,16 @@ done
 
 # ----------- ShadowrocketEx.conf ----------
 sed 's+^+IP-CIDR,+g; s+$+,no-resolve+g' chnroute.txt >$_path/.shadowrocket/cncidr.txt
+curl_githubusercontent https://raw.githubusercontent.com/Johnshall/Shadowrocket-ADBlock-Rules-Forever/release/sr_top500_banlist.conf >$_path/.shadowrocket/banlist.conf
+curl_githubusercontent https://raw.githubusercontent.com/Johnshall/Shadowrocket-ADBlock-Rules-Forever/release/sr_top500_whitelist.conf >$_path/.shadowrocket/whitelist.conf
+sed '/ios_rule_script/iDOMAIN-SET,https://raw.githubusercontent.com/honwen/openwrt-dnsmasq-extra/master/dnsmasq-extra/files/data/adblock.ext,REJECT' -i $_path/.shadowrocket/*list.conf
+sed 's+https://raw.githubusercontent.com+https://ghp.ci/https://raw.githubusercontent.com+g' -i $_path/.shadowrocket/*list.conf
+sed '/^#/d' -i $_path/.shadowrocket/*list.conf
+sed -i '/MITM/,+2d' -i $_path/.shadowrocket/*list.conf
+
+sed '/^[ \t]*$/d' -i $_path/.shadowrocket/*list.conf
+sed 's+^\[+\n[+g' -i $_path/.shadowrocket/*list.conf
+sed '/./,$!d' -i $_path/.shadowrocket/*list.conf
 # ----------- ShadowrocketEx.conf ----------
 
 cd -
