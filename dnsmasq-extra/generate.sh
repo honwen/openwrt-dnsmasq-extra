2c7178395b93c50b30c785084d7177af60470bc7
honwen.chan <honwen.chan@gmail.com>
2026-01-09 00:09:01 +0800

    update data: all @2025.1231

--- dnsmasq-extra/generate.sh
+++ dnsmasq-extra/generate.sh
@@ -87,7 +87,9 @@ time cidr-merger <<-EOF >chnroute.txt.new
 	$(for it in 3462 9269 9381 25820 31898 35916 36352 45090 45102 48266 49683 64050 132203 132591 135377 136038 136907 138915 141159; do
 		echo >&2 "ASN$it"
 
-		curl_githubusercontent https://raw.githubusercontent.com/cbuijs/ipasn/refs/heads/master/asn/as$it.list | grep -vE '^#' | grep -v ':'
+		curl_githubusercontent https://raw.githubusercontent.com/ipverse/as-ip-blocks/refs/heads/master/as/$it/ipv4-aggregated.txt | grep -vE '^#' | grep -v ':'
+
+		# curl_githubusercontent https://raw.githubusercontent.com/cbuijs/ipasn/refs/heads/master/asn/as$it.list | grep -vE '^#' | grep -v ':'
 
 		# curl -skSL -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64)' --speed-limit 50000 --speed-time 90 https://api.bgpview.io/asn/$it/prefixes | jq . >../../.asn/$it.json
 		# sleep 44
